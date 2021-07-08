74262cb05523498420482be3590962e73e9ca07f
Malaka98 <malaka.aruna14413@gmail.com>
2021-07-08 12:31:25 +0530

    Add project files.

---
+++ C_Sharp_Delegate/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Delegate
+{
+    class Calculator
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Mul(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Div(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ C_Sharp_Delegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace C_Sharp_Delegate
+{
+    delegate int Cal(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cal obj = Calculator.Add;
+            Console.WriteLine(obj(50, 10));
+
+            obj = Calculator.Div;
+            Console.WriteLine(obj(50, 10));
+
+            obj = Calculator.Mul;
+            Console.WriteLine(obj(50, 10));
+
+            obj = Calculator.Sub;
+            Console.WriteLine(obj(50, 10));
+        }
+    }
+}
